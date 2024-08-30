029ebbfc9600de0a4a0209523e266260141a1ef2
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-08-30 16:02:52 -0400

    stripe webhook

---
+++ app/api/payments/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(req: NextRequest) {
+  //webhook functionality
+  const payload = await req.text();
+
+  const sig = req.headers.get("stripe-signature");
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      payload,
+      sig!,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.completed": {
+        const session = await stripe.checkout.sessions.retrieve(
+          event.data.object.id,
+          {
+            expand: ["line_items"],
+          }
+        );
+        console.log({ session });
+
+        //connect to the db create or update user
+        break;
+      }
+      case "customer.subscription.deleted": {
+        // connect to db
+        const subscriptionId = event.data.object.id;
+        const subscription = await stripe.subscriptions.retrieve(
+          subscriptionId
+        );
+        console.log({ subscription });
+
+        //connect to db
+        //update users status to cancelled / revoke access
+        break;
+      }
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    return NextResponse.json({
+      status: "success",
+    });
+  } catch (err) {
+    return NextResponse.json({ status: "Failed", err });
+  }
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
